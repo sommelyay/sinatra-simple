5cf02f59cd102369b604aea31f8905c0f0421c80
Gerald van den Berg <sommelyay@gmail.com>
2013-11-26 01:21:14 -0500

    updating

--- app.rb
+++ app.rb
@@ -10,14 +10,8 @@ enable :sessions
 
 
 class Post < ActiveRecord::Base
- 	validates :title, presence: true, length: { minimum: 5 }
- 	validates :body, presence: true
-end
-
-get "/" do
-  @posts = Post.order("created_at DESC")
-  @title = "Welcome."
-  erb :"posts/index"
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true
 end
 
 helpers do
@@ -30,24 +24,48 @@ helpers do
   end
 end
 
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
 
+# get ALL posts
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome."
+  erb :"posts/index"
+end
+
+# create new post
 get "/posts/create" do
- @title = "Create post"
- @post = Post.new
- erb :"posts/create"
+  @title = "Create post"
+  @post = Post.new
+  erb :"posts/create"
 end
-
 post "/posts" do
- @post = Post.new(params[:post])
- if @post.save
-   redirect "posts/#{@post.id}", :notice => 'Congrats! Love the new post. (This message will disapear in 4 seconds.)'
- else
-   redirect "posts/create", :error => 'Something went wrong. Try again. (This message will disapear in 4 seconds.)'
- end
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}", :notice => 'Congrats! I love the new post. (This message will disapear in 4 seconds.)'
+  else
+    redirect "posts/create", :error => 'Something went wrong. Try again. (This message will disapear in 4 seconds.)'
+  end
 end
 
+# view post
 get "/posts/:id" do
- @post = Post.find(params[:id])
- @title = @post.title
- erb :"posts/view"
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/view"
+end
+
+# edit post
+get "/posts/:id/edit" do
+  @post = Post.find(params[:id])
+  @title = "Edit Form"
+  erb :"posts/edit"
+end
+put "/posts/:id" do
+  @post = Post.find(params[:id])
+  @post.update(params[:post])
+  redirect "/posts/#{@post.id}"
 end
